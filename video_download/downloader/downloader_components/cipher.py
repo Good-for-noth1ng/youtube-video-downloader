f45875942afead8ee34c39f42dfc8d8f0909d5c3
Good-for-noth1ng <apasnost@list.ru>
2022-05-24 18:56:42 +0200

    cipher

--- video_download/downloader/downloader_components/cipher.py
+++ video_download/downloader/downloader_components/cipher.py
@@ -4,3 +4,84 @@ from itertools import chain
 from typing import Any, Callable, Dict, List, Optional, Tuple
 
 from downloader.downloader_components.error import ExtractError, RegexMatchError
+from downloader.downloader_components.helpers import cache, regex_search
+from downloader.downloader_components.parser import find_object_from_startpoint, throttling_array_split
+
+logger = logging.getLogger(__name__)
+
+class Cipher:
+    def __init__(self, js: str):
+        pass
+
+    def calculate_n(self, initial_n: list):
+        pass
+
+    def get_signature(self, ciphered_signature: str):
+        pass
+
+    @cache
+    def parse_functions(self, js_func: str) -> Tuple[str, int]:
+        pass
+
+def get_initial_function_name(js: str) -> str:
+    pass
+
+def get_transform_plan(js: str) -> List[str]:
+    pass
+
+def get_transform_objects(js: str, var: str) -> List[str]:
+    pass
+
+def get_transform_map(js: str, var: str) -> Dict:
+    pass
+
+def throttling_function_name(js: str) -> str:
+    pass
+
+def throttling_function_code(js: str) -> str:
+    pass
+
+def get_throttling_function_array(js: str) -> List[Any]:
+    pass
+
+def get_throttling_plan(js: str):
+    pass
+
+def reverse(arr: List, _:Optional[Any]):
+    pass
+
+def splice(arr: list, b: int):
+    pass
+
+def swap(arr: List,  b: int):
+    pass
+
+def throttling_reverse(arr: list):
+    pass
+
+def throttling_push(d: list, e: Any):
+    pass
+
+def throttling_mod_func(d: list, e: int):
+    pass
+
+def throttling_unshift(d: list, e: int):
+    pass
+
+def throttling_cipher_function(d: list, e: str):
+    pass
+
+def throttling_nested_splice(d: list, e: int):
+    pass
+
+def throttling_prepend(d: list, e: int):
+    pass
+
+def throttling_swap(d: list, e: int):
+    pass
+
+def js_splice(arr: list, start: int, delete_count: None, *items):
+    pass
+
+def map_functions(js_func: str) -> Callable:
+    pass
--- video_download/downloader/downloader_components/parser.py
+++ video_download/downloader/downloader_components/parser.py
@@ -27,7 +27,7 @@ def parse_for_objects(html, preceding_regex):
     start_index = result.end()
     return parse_for_objects_from_startpoint(html, start_index)
 
-def parse_for_objects_from_startpoint(html, start_point):
+def find_object_from_startpoint(html, start_point):
     html = html[start_point:]
     if html[0] not in ['{', '[']:
         raise HTMLParseError(f'Invalid start point. Start of HTML:\n{html[:20]}')
@@ -61,3 +61,41 @@ def parse_for_objects_from_startpoint(html, start_point):
         i += 1
     full_obj = html[:i]
     return full_obj
+
+def parse_for_object_from_startpoint(html, start_point):
+    full_obj = find_object_from_startpoint(html, start_point)
+    try:
+        return json.loads(full_obj)
+    except json.decoder.JSONDecodeError:
+        try:
+            return ast.literal_eval(full_obj)
+        except (ValueError, SyntaxError):
+            raise HTMLParseError("Couldn't parse object")
+
+def throttling_array_split(js_array):
+    results = []
+    curr_substring = js_array[1:]
+
+    comma_regex = re.compile(r",")
+    func_regex = re.compile(r"function\([^)]*\)")
+
+    while len(curr_substring) > 0:
+        if curr_substring.startswith('function'):
+            match = func_regex.search(curr_substring)
+            match_start, match_end = match.span()
+
+            function_text = find_object_from_startpoint(curr_substring, match.span()[1])
+            full_function_def = curr_substring[:match_end + len(function_text)]
+            results.append(full_function_def)
+            curr_substring = curr_substring[len(full_function_def) + 1:]
+        else:
+            match = comma_regex.search(curr_substring)
+            try:
+                match_start, match_end = macth.span()
+            except AttributeError:
+                match_start = len(curr_substring) - 1
+                match_end = match_start + 1
+            curr_el = curr_substring[:match_start]
+            results.append(curr_el)
+            curr_substring = curr_substring[match_end:]
+    return results
