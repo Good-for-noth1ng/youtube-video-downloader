fb54b6277242e4a5ad030cb39d4263403db589c5
Good-for-noth1ng <apasnost@list.ru>
2022-06-12 22:32:31 +0200

    fix

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 release: python video_download/manage.py migrate --noinput
-web: python3 run_pooling.py 
-worker: python3 run_pooling.py
+web: python3 video_download/run_pooling.py 
+worker: python3 video_download/run_pooling.py
--- video_download/tgbot/dispatcher.py
+++ video_download/tgbot/dispatcher.py
@@ -135,10 +135,10 @@ except telegram.error.Unauthorized:
     logging.error(f"Invalid telegram token")
     sys.exit(1)
 
-# @app.task(ignore_result=True)
-# def process_telegram_event(update_json):
-#     update = Update.de_json(update_json, bot)
-#     dispatcher.process_update(update)
+@app.task(ignore_result=True)
+def process_telegram_event(update_json):
+    update = Update.de_json(update_json, bot)
+    dispatcher.process_update(update)
 
 def set_up_commands(bot_instance: Bot) -> None:
     langs_with_commands: Dict[str, Dict[str, str]] = {
