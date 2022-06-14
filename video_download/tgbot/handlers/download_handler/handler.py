f1c4f49919d77576ac7ad6a5e0b8fdbfa87647cc
Good-for-noth1ng <apasnost@list.ru>
2022-06-14 21:02:57 +0200

    fix

--- video_download/tgbot/handlers/download_handler/handler.py
+++ video_download/tgbot/handlers/download_handler/handler.py
@@ -8,7 +8,6 @@ from youtube_crawler.pytube.contrib.playlist import Playlist
 from telegram import ParseMode, ReplyKeyboardRemove, Update
 from telegram.ext import CallbackContext, ConversationHandler
 
-
 import tgbot.handlers.download_handler.conversation_state as conversation_state
 import tgbot.handlers.download_handler.static_text as static_text
 import tgbot.handlers.download_handler.keyboards as keyboards
