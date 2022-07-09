9403c3043a909a3c41dadbd78496c11a0cb0abcf
Good-for-noth1ng <apasnost@list.ru>
2022-07-09 16:59:44 +0200

    channel

--- video_download/tgbot/handlers/channel_handler/conversation_states.py
+++ video_download/tgbot/handlers/channel_handler/conversation_states.py
@@ -1,1 +1,2 @@
 SEND_CHANNEL_STATE = "sendchannelstate"
+ASK_QUALITY_CHANNEL_VIDEO = "askqualitychannelvideo"
--- video_download/tgbot/handlers/channel_handler/handler.py
+++ video_download/tgbot/handlers/channel_handler/handler.py
@@ -5,6 +5,8 @@ from youtube_crawler.pytube.contrib.channel import Channel
 
 import tgbot.handlers.channel_handler.static_text as st
 import tgbot.handlers.channel_handler.conversation_states as cs
+from tgbot.handlers.search_handler.handler import get_video_length
+from tgbot.handlers.search_handler import keyboards as search_keyboards
 
 def ask_channel(update: Update, context: CallbackContext):
     update.message.reply_text(st.send_chanel_url_text)
@@ -14,5 +16,14 @@ def extract_channel_video(update: Update, context: CallbackContext):
     url: str = update.message.text
     channel = Channel(url)
     for video in channel.videos:
-        print(video.author, video.title)
+        video_id = video.video_id
+        author = video.author
+        title = video.title
+        video_length = get_video_length(length_in_seconds=video.length)
+        message_text = f"🎥 {author}: ▶️ {title}, 🕐 {video_length}"   
+        update.message.reply_text(
+            text=message_text, 
+            reply_markup=search_keyboards.make_keyboard_to_ask_video(video_id)
+        )
+    return cs.ASK_QUALITY_CHANNEL_VIDEO     
     
